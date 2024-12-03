711bc6f27bcc7306f8f09d7abbee953076cb5106
Kostas Filippopolitis <kfilippopolitis@athenarc.gr>
2024-12-03 19:46:02 +0200

    Improvements

--- frontend/src/app/pages/federations-page/federation-card/federation-card.component.ts
+++ frontend/src/app/pages/federations-page/federation-card/federation-card.component.ts
@@ -1,10 +1,14 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Federation } from '../../../interfaces/federations.interface';
+import {SlicePipe} from "@angular/common";
 
 @Component({
   selector: 'app-federation-card',
   templateUrl: './federation-card.component.html',
   styleUrls: ['./federation-card.component.css'],
+  imports: [
+    SlicePipe
+  ],
   standalone: true
 })
 export class FederationCardComponent {
@@ -13,6 +17,7 @@ export class FederationCardComponent {
   @Output() updateFederation = new EventEmitter<string>();
   @Output() deleteFederation = new EventEmitter<string>();
   @Output() visualizeDataModel = new EventEmitter<string>();
+  showMore: boolean = false;
 
   viewDataModel() {
     this.visualizeDataModel.emit(this.federation.code);
@@ -24,5 +29,9 @@ export class FederationCardComponent {
 
   delete() {
     this.deleteFederation.emit(this.federation.code);
+  }
+
+  toggleDescription(): void {
+    this.showMore = !this.showMore;
   }
 }
